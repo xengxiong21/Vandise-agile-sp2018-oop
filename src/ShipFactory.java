fec67fc27dd16672795e7297c23d971ae428fbee
Xiong <xxiong4@student.cvtc.edu>
2018-03-07 13:28:21 -0600

    test 1

--- src/ShipFactory.java
+++ src/ShipFactory.java
@@ -11,6 +11,9 @@ public class ShipFactory {
 		case BATTLESHIP:
 			ship = new Ship(4);
 			break;
+		case PATROL:
+			ship = new Ship(2);
+			break;
 		default:
 			throw new Exception("Undefined ShipType: " + type);
 		}
