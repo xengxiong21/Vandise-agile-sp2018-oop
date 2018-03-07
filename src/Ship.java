95281b5cf3db82fc819874521b8d6d6ec86cefce
Xiong <xxiong4@student.cvtc.edu>
2018-03-07 14:06:09 -0600

    ship test

--- src/Ship.java
+++ src/Ship.java
@@ -28,11 +28,11 @@ public class Ship implements Observable {
 	//	a peice of logic is missing here
 	//
 	public void hit() throws Exception {
-		if (this.life == 0) {
-			this.sunk = true;
-		}
 		if (!this.sunk) {
 			this.life -= 1;
+			if (this.life == 0) {
+				this.sunk = true;
+			}
 		} else {
 			throw new Exception("Hit on ship that's already sunk.");
 		}
